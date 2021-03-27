ec875fc745bb88727b37f8ddd9b58e15c44c537d
Minh Hoàng <65158586+mhoang0304@users.noreply.github.com>
2021-03-28 00:30:17 +0700

    fix css

--- public/main.js
+++ public/main.js
@@ -4,31 +4,32 @@ const USER_API = API + "users"
 $(document).ready(function () {
 
     function loadDoc() {
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
                 // document.getElementById("name").innerHTML = this.responseText;
                 let user = JSON.parse(this.responseText);
 
                 $("#name").text(user[0].name)
                 for (let i = 0; i < user.length; i++) {
-                    $("table").append(`<tr>
-                        <td> ${user[i].name}</td>
-                        <td> ${user[i].birthday}</td>
-                        <td> ${user[i].email}</td>
-                        <td> ${user[i].phone}</td>  
-                        <td>
-                            <button class="detail">
-                                <a href="./detail.html?${user[i].id}">Chỉnh sửa</a>
-                            </button>
-                            <button onclick='deleteValue(${user[i].id})'>Xoá</button>
-                        </td>
-                    </tr>`)
+                    $("tbody").append(
+                        `<tr>
+                            <td>${user[i].name}</td>
+                            <td>${user[i].birthday}</td>
+                            <td>${user[i].email}</td>
+                            <td>${user[i].phone}</td>  
+                            <td>
+                                <button class="detail">
+                                    <a href="./detail.html?${user[i].id}">Chỉnh sửa</a>
+                                </button>
+                                <button onclick='deleteValue(${user[i].id})'>Xoá</button>
+                            </td>
+                        </tr>`)
                 }
             }
         };
-        xhttp.open("GET", USER_API, true);
-        xhttp.send();
+        request.open("GET", USER_API, true);
+        request.send();
     }
 
     loadDoc();
@@ -47,23 +48,18 @@ $(document).ready(function () {
             data: data
         }).done(function () {
             alert("Thêm thành công");
-            location.href = "/";
+            location.href = "./index.html";
         }).fail(function (err) {
             alert("Nhập lại thông tin");
         })
     })
-
 })
 
 function deleteValue(id) {
-    console.log(id);
-
     $.ajax({
         type: "DELETE",
         url: USER_API + "/" + id
     }).done(function () {
-        //SAu khi xoas xong 
-
+        document.body.onload;
     })
-
 };
